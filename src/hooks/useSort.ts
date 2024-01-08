3d833c2ba9d8ecf7ab3fc764b21a8204795ca6b1
wSeungMi <seungmi.dev@gmail.com>
2024-01-08 11:47:02 +0900

    Refactor: 판매자 주문내역 API 수정 및 useSort API 통신 분기처리 #123

--- src/hooks/useSort.ts
+++ src/hooks/useSort.ts
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import { IProduct } from '../type';
 import { api } from '../api/api';
 
 export const useSort = (products: IProduct[], initialSortOrder: string) => {
   const [sortedProducts, setSortedProducts] = useState(products);
   const [currentSortOrder, setCurrentSortOrder] = useState(initialSortOrder);
+  const location = useLocation();
+  const path = location.pathname;
 
   useEffect(() => {
     if (!Array.isArray(products)) {
@@ -28,15 +31,19 @@ export const useSort = (products: IProduct[], initialSortOrder: string) => {
         break;
     }
 
-    const sortFetchProducts = async () => {
+    // path === '/'면 getProductList, 'order'이면 getOrderState
+    const sortFetchProducts = async (path: string) => {
       try {
-        const response = await api.getProductList(sortQuery);
+        const response =
+          path === '/'
+            ? await api.getProductList(sortQuery)
+            : await api.getOrderState(sortQuery);
         setSortedProducts(response.data.item);
       } catch (error) {
         console.log('데이터를 받아오지 못했습니다.', error);
       }
     };
-    sortFetchProducts();
+    sortFetchProducts(path);
   }, [products, currentSortOrder]);
 
   return [sortedProducts, setCurrentSortOrder];
