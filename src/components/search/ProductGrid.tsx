3d33374eef5ea08169aca9d4457205f4ece69446
wSeungMi <seungmi.dev@gmail.com>
2024-01-08 17:54:19 +0900

    Refactor: reactQuery 사용을 위한 코드 주석처리 #123

--- src/components/search/ProductGrid.tsx
+++ src/components/search/ProductGrid.tsx
@@ -1,5 +1,5 @@
 import { Grid, Typography, Box, Grow } from '@mui/material';
-import { Skeleton } from '@mui/material';
+// import { Skeleton } from '@mui/material';
 import { IProduct } from '../../type';
 import { ProductCard } from '../../pages/product/ProductCard';
 
@@ -24,19 +24,19 @@ export const ProductGrid = ({
     return { xs: 12, sm: 6, md: 4, lg: 4, xl: 4 };
   };
 
-  const ProductSkeleton = () => {
-    return (
-      <Grid container spacing={4} m={4}>
-        {Array.from(new Array(itemsPerPage)).map((_, index) => (
-          <Grid item key={index} {...getItemSize()}>
-            <Skeleton variant="rectangular" height={200} />
-            <Skeleton variant="text" />
-            <Skeleton variant="text" />
-          </Grid>
-        ))}
-      </Grid>
-    );
-  };
+  // const ProductSkeleton = () => {
+  //   return (
+  //     <Grid container spacing={4} m={4}>
+  //       {Array.from(new Array(itemsPerPage)).map((_, index) => (
+  //         <Grid item key={index} {...getItemSize()}>
+  //           <Skeleton variant="rectangular" height={200} />
+  //           <Skeleton variant="text" />
+  //           <Skeleton variant="text" />
+  //         </Grid>
+  //       ))}
+  //     </Grid>
+  //   );
+  // };
 
   const renderNoProductsMessage = () => {
     if (isDataFetched && filteredProducts.length === 0) {
