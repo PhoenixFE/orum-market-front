547b9ba45579194cf76a2a579ea725ef0958e52a
wSeungMi <seungmi.dev@gmail.com>
2024-01-10 22:21:20 +0900

    Refactor: categoryQuery 추가 #129

--- src/hooks/useFilter.ts
+++ src/hooks/useFilter.ts
@@ -11,12 +11,12 @@ export const useFilter = (
     initialSelectedCategory,
   );
 
+  let categoryQuery = `custom={"extra.category.1": "${selectedCategory}"}`;
+
   useEffect(() => {
     const fetchFilterProducts = async () => {
       try {
-        const response = await api.getProductList(
-          `custom={"price":{"$lte":100000}}`,
-        );
+        const response = await api.getProductList(categoryQuery);
         console.log('응답', response.data.item);
       } catch (error) {
         console.log('데이터를 받아오지 못했습니다.', error);
