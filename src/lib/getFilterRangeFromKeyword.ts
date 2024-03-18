94cadc2d7a04dacf3361ab2de37c8f1cdc3c5def
wSeungMi <seungmi.dev@gmail.com>
2024-03-18 17:32:21 +0900

    Refactor: filterQuery 타입 선언 #129

--- src/lib/getFilterRangeFromKeyword.ts
+++ src/lib/getFilterRangeFromKeyword.ts
@@ -4,7 +4,23 @@ export default function getFilterRangeFromKeyword(
   queryKey: string,
   filterName: string,
 ) {
-  let filterQuery = {};
+  let filterQuery: {
+    allQuery?: {
+      queryKey?: string;
+      filterName?: string;
+    };
+    categoryQuery?: {
+      category?: string;
+    };
+    priceQuery?: {
+      minPrice?: number;
+      maxPrice?: number;
+    };
+    shippingFeeQuery?: {
+      minShippingFees?: number;
+      maxShippingFees?: number;
+    };
+  } = {};
   if (filterName !== 'all' && filterName !== '전체') {
     if (queryKey === 'category') {
       filterQuery = {
